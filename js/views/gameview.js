611377292258abc15f9bfdda53bff1c6ea94b827
Blake Johnson <blake41@gmail.com>
2015-05-06 18:05:44 -0400

    removed solution

--- js/views/gameview.js
+++ js/views/gameview.js
@@ -6,71 +6,29 @@
 		},
 
 		initialize : function() {
-			this.game = new app.Game();
-			this.game.on("X", this.drawX, this);
-			this.game.on("O", this.drawO, this);
-			this.game.on("tie", this.tie, this);
-			this.game.on("win", this.win, this);
-			this.render();
-			this.addIds();
+
 		},
 
 		render : function() {
-			var rows = this.createRowArray(3, '<tr>');
-			var cells = this.createColumnArray(3, '<td></td>');
-			this.$el.append("<table border='1' cellpadding='40'></table>")
-			this.$el.find('table').append(rows);
-			this.$el.find('tr').append(cells);
-			$("#container").append(this.$el)
 		},
 
 		addIds : function() {
-		  $('td').each(function(i, cell) {
-		    $(cell).attr('id', i);
-		  })
+
 		},
 
-		createRowArray : function(size, element) {
-		  return this.createArray(size, element)
-		},
+		handleClick : function() {
 
-		createColumnArray : function(size, element) {
-		  return this.createArray(size, element)
-		},
-
-		createArray : function(size, element) {
-		  return Array.apply(null, new Array(size)).map(String.prototype.valueOf,element)
-		},
-
-		handleClick : function(e) {
-			var id = Number($(e.target).attr("id"));
-			this.game.doTurn(id);
-		},
-
-		drawX : function(id) {
-			this.drawMark(id, "X");
-		},
-
-		drawMark : function(id, mark) {
-			this.$("#" + id).html(mark);
-		},
-		
-		drawO : function(id) {
-			this.drawMark(id, "O");
 		},
 
 		tie : function() {
-			alert("You Tied!");
-			this.clearBoard();
+	
 		},
 
-		win : function(player) {
-			alert("Player " + player + " Won!");
-			this.clearBoard();
+		win : function() {
+
 		},
 
 		clearBoard : function() {
-			$("td").html("");
 		}
 	})
 })();
