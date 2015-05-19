28b9eb176b6f4363660c8c32c843119f4708e8c0
Katie Hoffman <ktahoffman@gmail.com>
2015-05-19 13:27:35 -0400

    remove passing code

--- js/models/game.js
+++ js/models/game.js
@@ -17,84 +17,25 @@
         [2,5,8], 
         [0,4,8],
         [2,4,6]
-      ],
-      wayGameEnded: null,
-      pastBoard: null
+      ]
     }},
     updateState: function(id) {
-      var tempBoard = this.get("board");
-      tempBoard[id] = this.player();
-      this.set("board", tempBoard);
-      this.trigger(this.player(), id);
+      // your code here
     },
     tie: function() {
-      var spotsFilled = 0;
-      this.get("board").forEach(function(element) {
-        if (element) spotsFilled += 1;
-      });
-      return spotsFilled >= 9;
+      // your code here
     },
     gameOver: function() {
-      if (this.winnerCheck()) {
-        this.set("wayGameEnded", "win");
-        return true;  
-      } else if (this.tie()) {
-        this.set("wayGameEnded", "tie");
-        return true;
-      } else {
-        return false;
-      }
-    },
-    formatPastGame: function() {
-      var past = this.get("pastBoard");
-      var temp = [];
-      past.forEach(function(element, i) {
-        if (typeof element == "string") {
-          temp.push(element);
-        } else {
-          temp.push("-");
-        }
-        if (i == 2 || i == 5) temp.push("<br>");
-      });
-      return temp;
+      // your code here
     },
     player: function() {
-      return this.get("turn") % 2 == 0 ? "X": "O"
-    },
-    playerLocations: function() {
-      var locations = [];
-      var playerz = this.player();
-      var tempBoard = this.get("board");
-      tempBoard.forEach(function(piece, i) {
-        if (piece == playerz) locations.push(i);
-      });
-      return locations;
+      // your code here
     },
     winnerCheck: function() {
-      var locations = this.playerLocations();
-      var winner = false;
-      this.get("winningCombos").forEach(function(combo) {
-        var wonThis = true;
-        combo.forEach(function(id) {
-          if ($.inArray(id, locations) < 0) wonThis = false;
-        });
-        if (wonThis) winner = true;
-      });
-      return winner;
-    },
-    incrementTurn: function() {
-      var tempTurn = this.get("turn");
-      this.set("turn", tempTurn + 1);
-      return this.get("turn");
+      // your code here
     },
     doTurn: function(id) {
-      this.updateState(id);
-      if (this.gameOver()) {
-        var tieOrWin = this.get("wayGameEnded");
-        this.trigger(tieOrWin, this.player());
-      } else {
-        this.incrementTurn();
-      }
+      // your code here
     } 
-  })
+  });
 })();
--- js/views/gameview.js
+++ js/views/gameview.js
@@ -5,56 +5,25 @@
     },
     initialize: function() {
       this.game = new app.Game();
-      this.game.on("X", this.drawX, this);
-      this.game.on("O", this.drawO, this);
-      this.game.on("tie", this.tie, this);
-      this.game.on("win", this.win, this);
-      this.render();
+      // your code here
     },
     render: function() {
-      var html = '<div id="message"></div><table border="1" cellpadding="40"><tbody><tr><td id="0"></td><td id="1"></td><td id="2"></td></tr><tr><td id="3"></td><td id="4"></td><td id="5"></td></tr><tr><td id="6"></td><td id="7"></td><td id="8"></td></tr></tbody></table><button id="lastGame">Show Me Last Games Results!</button><div id="lastGameBox"></div></body>';
-      this.$el.append(html);
-      $("#container").append(this.$el);
+      // your code here
     },
     handleClick: function(e) {
-      var idString = e.target.id;
-      if (idString == "lastGame") {
-        this.drawLastGame()
-      } else {
-        var id = Number(idString);
-        this.game.doTurn(id);
-      }
-    },
-    drawLastGame: function() {
-      var html = this.game.formatPastGame();
-      this.$("#lastGameBox").html(html);
+      // your code here
     },
     drawX: function(id) {
-      this.drawMark(id, "X");
+      // your code here
     },
     drawO: function(id) {
-      this.drawMark(id, "O");
-    },
-    drawMark: function(id, mark) {
-      this.$("#" + id).html(mark);
+      // your code here
     },
     message: function(message) {
       this.$("#message").html(message)
     },
-    tie: function() {
-      this.message("Tie game");
-      this.clearBoard();
-    },
-    win: function() {
-      this.message("Player " + this.game.player() + " Wins!");
-      this.clearBoard();
-    },
     clearBoard: function() {
-      this.$("td").html("");
-      var tempBoard = this.game.get("board");
-      this.game.set("pastBoard", tempBoard);
-      this.game.set("board", [null, null, null, null, null, null, null, null, null]);
-      this.game.set("turns", 0);
+      // your code here
     }
-  })
+  });
 })();
