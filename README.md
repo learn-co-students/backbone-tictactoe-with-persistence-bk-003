7e5a16b24ddd3bded62db947a4cc66b7b4bf33e4
Katie Hoffman <ktahoffman@gmail.com>
2015-05-19 11:43:58 -0400

    add .learn, specs from first lab, update frontmatter

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-This is an extension to our last backbone lab.  Here we're going to persist the last games state to our model just like we did in the jquery lab.  However, in this lab rather than saving the state of the game to the DOM (which was a bad solution) let's use the game model.  Add a new attribute to the game model and save the games state there once someone has won or tied.  Add a button to the DOM just like in the jquery example that allows someone to click and have an alert pop up with the text of the last game. The tests won't give you that much guidance as to how to do this, other than verifying that when we click that button we get the right output.  How you implement that functionality is up to you. 
+# Backbone Tic-Tac-Toe with Persistence
+
+## About
+
+This is an extension to our last backbone lab.  Here we're going to persist the last games state to our model just like we did in the jquery lab.  However, in this lab we're going to use the game model to save the last game's state.
+
+## Instructions
+
+Add a new attribute to the game model and save the games state there once someone has won or tied.  Add a button to the DOM just like in the jquery example that allows someone to click and have an alert pop up with the text of the last game. The tests won't give you that much guidance as to how to do this, other than verifying that when we click that button we get the right output.  How you implement that functionality is up to you.
--- spec/gameview.spec.js
+++ spec/gameview.spec.js
@@ -1,10 +1,9 @@
 'use strict';
-describe('gameView', function() {
 
-
+describe('board', function() {
   describe( "#initialize", function() {
     it("should set the game property of the view to be an instance of the game model", function() {
-      var gameView = new app.GameView;
+      var gameView = new app.GameView();
       expect(gameView.game).not.toBeUndefined();
     });     
   });
@@ -16,28 +15,19 @@ describe('gameView', function() {
         var gameView = new app.GameView;
         gameView.game.trigger("X");
         expect(spy).toHaveBeenCalled();
-      });     
-    });
-
-    describe( "#initialize", function() {
+      });
       it("should wire up the O event listeners on the model", function() {
         var spy = spyOn(app.GameView.prototype, "drawO");
         var gameView = new app.GameView;
         gameView.game.trigger("O");
         expect(spy).toHaveBeenCalled();
-      });     
-    });
-
-    describe( "#initialize", function() {
+      });
       it("should wire up the tie event listeners on the model", function() {
         var spy = spyOn(app.GameView.prototype, "tie");
         var gameView = new app.GameView;
         gameView.game.trigger("tie");
         expect(spy).toHaveBeenCalled();
-      });     
-    });
-
-    describe( "#initialize", function() {
+      });
       it("should wire up the win event listeners on the model", function() {
         var spy = spyOn(app.GameView.prototype, "win")
         var gameView = new app.GameView;
@@ -54,14 +44,6 @@ describe('gameView', function() {
     });
     describe( "#initialize", function() {
       it("should call render", function() {
-        spyOn(gameView, "addIds");
-        gameView.initialize();
-        expect(gameView.addIds).toHaveBeenCalled();
-      });     
-    });
-
-    describe( "#initialize", function() {
-      it("should add the ids to the dom", function() {
         spyOn(gameView, "render");
         gameView.initialize();
         expect(gameView.render).toHaveBeenCalled();
@@ -77,7 +59,7 @@ describe('gameView', function() {
     });
     describe( "#render & addIds", function() {
       it("should create a board in HTML when its initialized", function() {
-        expect($("#container")).toContainHtml('<table border="1" cellpadding="40"><tbody><tr><td id="0"></td><td id="1"></td><td id="2"></td></tr><tr><td id="3"></td><td id="4"></td><td id="5"></td></tr><tr><td id="6"></td><td id="7"></td><td id="8"></td></tr></tbody></table>');
+        expect($("#container")).toContainHtml('<div id="message"></div><table border="1" cellpadding="40"><tbody><tr><td id="0"></td><td id="1"></td><td id="2"></td></tr><tr><td id="3"></td><td id="4"></td><td id="5"></td></tr><tr><td id="6"></td><td id="7"></td><td id="8"></td></tr></tbody></table>');
       });     
     });
 
@@ -99,32 +81,161 @@ describe('gameView', function() {
   describe( "#handleClick", function() {
     it("should tell the game what element was clicked", function() {
       setFixtures('<div id="container"></div>'); 
-      var spy = spyOn(app.Game.prototype, "doTurn")
+      var spy = spyOn(app.Game.prototype, "doTurn");
       var gameView = new app.GameView;
-      $("#3").click()
+      $("#3").click();
       expect(spy).toHaveBeenCalledWith(3)    
-    });     
+    });
+
+    it("the game's board matches the table", function() {
+      setFixtures('<div id="container"></div>'); 
+      var gameView = new app.GameView;
+      $("#0").click();
+      $("#1").click();
+      $("#4").click();
+      $("#8").click();
+      $("#2").click();
+      $("#6").click();
+      $("#7").click();
+      $("#3").click();
+      var tempBoard = gameView.game.get("board");
+      [0,4,2,7].forEach(function(space) {
+        expect(tempBoard[space]).toEqual("X");
+        expect($("#" + space).text()).toEqual("X");
+      });
+      [1,8,6,3].forEach(function(space) {
+        expect(tempBoard[space]).toEqual("O");
+        expect($("#" + space).text()).toEqual("O");
+      });
+    });
+  });
+
+  describe( "#win", function() {
+    it("gets called when there is a winner", function() {
+      setFixtures('<div id="container"></div>');
+      var spy = spyOn(app.GameView.prototype, "win");
+      var gameView = new app.GameView;
+      $("#0").click();
+      $("#3").click();
+      $("#1").click();
+      $("#4").click();
+      $("#2").click();
+      expect(spy).toHaveBeenCalled()    
+    });
+    it("displays a message about Player X in the message div on horizontal win", function() {
+      setFixtures('<div id="container"></div>');
+      var gameView = new app.GameView;
+      $("#0").click();
+      $("#3").click();
+      $("#1").click();
+      $("#4").click();
+      $("#2").click();
+      expect($("#message").text()).toEqual("Player X Wins!");  
+    });
+    it("displays a message about Player O in the message div on diagonal win", function() {
+      setFixtures('<div id="container"></div>');
+      var gameView = new app.GameView;
+      $("#3").click();
+      $("#0").click();
+      $("#1").click();
+      $("#4").click();
+      $("#2").click();
+      $("#8").click();
+      expect($("#message").text()).toEqual("Player O Wins!");  
+    });
+    it("clears the table after a person wins, resets turns to 0, clears the board", function() {
+      setFixtures('<div id="container"></div>');
+      var gameView = new app.GameView;
+      $("#3").click();
+      $("#0").click();
+      $("#1").click();
+      $("#4").click();
+      $("#2").click();
+      $("#8").click();
+      expect($("#message").text()).toEqual("Player O Wins!");
+      for(var i=0;i<9;i++){
+        expect($("#" + i).text()).toEqual("");
+      }
+      var tempBoard = gameView.game.get("board");
+      tempBoard.forEach(function(square) {
+        expect(square).toEqual(null);
+      });
+      expect(gameView.game.get("turns")).toEqual(0);
+    });
+  });
+  describe( "#tie", function() {
+   it("displays a message the tie in the message div", function() {
+      setFixtures('<div id="container"></div>');
+      var gameView = new app.GameView;
+      $("#0").click();
+      $("#1").click();
+      $("#4").click();
+      $("#8").click();
+      $("#2").click();
+      $("#6").click();
+      $("#7").click();
+      $("#3").click();
+      $("#5").click();
+      expect($("#message").text()).toEqual("Tie game");  
+    });
+    it("clears the table after a person wins, resets turns to 0, clears the board", function() {
+      setFixtures('<div id="container"></div>');
+      var gameView = new app.GameView;
+      $("#0").click();
+      $("#1").click();
+      $("#4").click();
+      $("#8").click();
+      $("#2").click();
+      $("#6").click();
+      $("#7").click();
+      $("#3").click();
+      $("#5").click();
+      expect($("#message").text()).toEqual("Tie game");
+      for(var i=0;i<9;i++){
+        expect($("#" + i).text()).toEqual("");
+      }
+      var tempBoard = gameView.game.get("board");
+      tempBoard.forEach(function(square) {
+        expect(square).toEqual(null);
+      });
+      expect(gameView.game.get("turns")).toEqual(0);
+    });
   });
 
   describe( "#showLastGame", function() {
     it("should persist the last game to the model", function() {
       setFixtures('<div id="container"></div>'); 
-      spyOn(window, "alert")
-      // spyOn(app.GameView.prototype)
       var gameView = new app.GameView;
-      // X goes
       gameView.game.doTurn(0);
-      // O goes
       gameView.game.doTurn(1);
-      // X goes
       gameView.game.doTurn(4);
-      // O goes
       gameView.game.doTurn(2);
-      // X goes
+      // _X_|_O_|_O_
+      // ___|_X_|___
+      //    |   |   
       gameView.game.doTurn(8);
-      $("#lastGame").click()
-      expect(window.alert).toHaveBeenCalledWith("XOO\n-X-\n--X\n");
-    });     
+      // _X_|_O_|_O_
+      // ___|_X_|___
+      //    |   | X 
+      expect($("#message").html()).toEqual("Player X Won!");
+      // ___|___|___
+      // ___|___|___
+      //    |   |  
+      $("td").each(function() {
+        expect($(this).html()).toEqual("");
+      });
+      gameView.game.doTurn(8);
+      gameView.game.doTurn(0);
+      gameView.game.doTurn(6);
+      gameView.game.doTurn(7);
+      // _O_|___|___
+      // ___|___|___
+      //  X | O | X
+      $("#lastGame").click();
+      // X O O
+      // - X -
+      // - - X
+      expect($("#lastGameBox").html()).toEqual("XOO<br>-X-<br>--X");
+    });
   });
-
 });
